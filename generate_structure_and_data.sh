dec1e4b4059936d32f08b3c0ac931b15397c95cd
Валентин Сыров <vs.syrov@gmailc.com>
2021-03-13 14:03:06 +0300

    Создан сервис

--- generate_structure_and_data.sh
+++ generate_structure_and_data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-cd $1
+start_dir=$PWD
+db_project_path=$1/src/main/resources/db/
+
+cd $db_project_path
+
 
 export PGPASSWORD="docker"
 
 dropdb -h localhost -p 5432 -U postgres  postgres
 createdb -h localhost -p 5432 -U postgres  postgres
-psql -U postgres -d postgres -h localhost -p 5432 -a -f $1.sql
+psql -U postgres -d postgres -h localhost -p 5432 -a -f init.sql
 
-liquibase --overwriteOutputFile=true  --diffTypes=tables,functions,views,columns,indexes,foreignkeys,primarykeys,uniqueconstraints,data,storedprocedure,triggers,sequences --changeLogFile=db.changelog-master.xml generateChangeLog
+liquibase --defaultsFile=$start_dir/liquibase.properties --overwriteOutputFile=true  --diffTypes=tables,functions,views,columns,indexes,foreignkeys,primarykeys,uniqueconstraints,data,storedprocedure,triggers,sequences --changeLogFile=$db_project_path/changelog/db.changelog-master.xml generateChangeLog
 
--- personal-information/generate_structure_and_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-liquibase --diffTypes=tables,functions,views,columns,indexes,foreignkeys,primarykeys,uniqueconstraints,data,storedprocedure,triggers,sequences --changeLogFile=personal_information_initial.xml generateChangeLog
-
