288c99dcf3f921ac0c5b9d5e23ed9124e5c3a9aa
Валентин Сыров <vs.syrov@gmailc.com>
2021-03-16 23:53:59 +0300

    Controller renamed

--- results-control-api-gateway/src/main/java/com/cust_trial/journal/resultscontrolapigateway/ApiGatewayController.java
+++ results-control-api-gateway/src/main/java/com/cust_trial/journal/resultscontrolapigateway/ApiGatewayController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 
 @RestController
-public class Controller {
+public class ApiGatewayController {
 
     @Autowired
     PersonalInfoClient personalInfoClient;
