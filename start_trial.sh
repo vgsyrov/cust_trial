9347c386fd99d04357f2509feffb3be9e8fa547a
Валентин Сыров <vs.syrov@gmailc.com>
2021-03-24 22:18:04 +0300

    One command to start

--- start_trial.sh
+++ start_trial.sh
@@ -5,5 +5,4 @@
 #docker rmi -f $(docker images -a -q)
 
 #./build_java.sh
-docker-compose -f docker-compose_full.yml build --parallel #--no-color > docker.log
-docker-compose -f docker-compose_full.yml up --quiet-pull #--build #--no-color > docker.log
+docker-compose -f docker-compose_full.yml build --parallel && docker-compose -f docker-compose_full.yml up --quiet-pull
